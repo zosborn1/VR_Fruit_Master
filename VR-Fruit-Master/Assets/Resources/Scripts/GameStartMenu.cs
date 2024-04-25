a8dad1ab808c34447c362def101706a7a902deee
zosborn1 <96489409+zosborn1@users.noreply.github.com>
2024-04-25 14:10:39 -0500

    pushing

--- VR-Fruit-Master/Assets/Resources/Scripts/GameStartMenu.cs
+++ VR-Fruit-Master/Assets/Resources/Scripts/GameStartMenu.cs
@@ -6,9 +6,6 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class GameStartMenu : MonoBehaviour
 {
-    // public GameObject weaponSelectionPanel;
-    // public GameObject rangeSelectionPanel;
-    // public GameObject mainMenuPanel;
 
     [Header("Buttons")]
     public Button startButton;
@@ -16,21 +13,11 @@ public class GameStartMenu : MonoBehaviour
     public TMP_Dropdown leftWeaponDropdown;
     public TMP_Dropdown rightWeaponDropdown;
     public GameObject rangeTitle;
-    // public Button weaponsButton;
-    // public Button rangeButton;
-    // public Button aboutButton;
-    // public Button quitButton;
+
 
     void Start()
     {
-        // Button Listeners
-        // startButton.onClick.AddListener(StartGame);
-        // quitButton.onClick.AddListener(QuitGame);
 
-        // // Placeholder listeners for the other buttons
-        // weaponsButton.onClick.AddListener(OpenWeaponsMenu);
-        // rangeButton.onClick.AddListener(OpenRangeMenu);
-        // aboutButton.onClick.AddListener(OpenAboutMenu);
         VariableHolder.range = 60;
         VariableHolder.left_weapon = 0;
         VariableHolder.right_weapon = 0;
@@ -54,36 +41,9 @@ public class GameStartMenu : MonoBehaviour
     {
         VariableHolder.right_weapon = (int)rightWeaponDropdown.value;
     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
 
-//     public void QuitGame()
-//     {
-//         Debug.Log("Quitting the game...");
-//         Application.Quit();
-//     }
-
-//     // Placeholder methods for the other buttons
-//     public void OpenWeaponsMenu()
-//     {
-//           Debug.Log("Opening weapon selection...");
-//         mainMenuPanel.SetActive(false);
-//     weaponSelectionPanel.SetActive(true);
-// }
-
-//     public void OpenRangeMenu()
-//    {
-//     Debug.Log("Opening range selection...");
-//     mainMenuPanel.SetActive(false);
-//     rangeSelectionPanel.SetActive(true);
-// }
-// public void ReturnToMainMenu()
-// {
-//     weaponSelectionPanel.SetActive(false);
-//     rangeSelectionPanel.SetActive(false);
-//     mainMenuPanel.SetActive(true);
-// }
-
-//     public void OpenAboutMenu()
-//     {
-//         Debug.Log("About button clicked. Functionality not implemented yet.");
-//     }
 }
