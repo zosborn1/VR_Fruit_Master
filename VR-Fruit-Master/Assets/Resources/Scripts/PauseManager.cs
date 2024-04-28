d5d00979eddbed1853484cda14fb1ac6341464d3
Jane Park <157393457+syjanepark@users.noreply.github.com>
2024-04-28 15:22:17 -0500

    pause/resume/main menu

---
+++ VR-Fruit-Master/Assets/Resources/Scripts/PauseManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        // Check if the specified controller button is pressed
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene("StartScene"); // Replace "MainMenuScene" with the actual name of your main menu scene
+    }
+}
